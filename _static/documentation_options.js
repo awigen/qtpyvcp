b2f8b2353c53b0a5f8f643968f2789efec086428
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-26 13:31:26 +0000

    Deploy kcjengr/qtpyvcp to github.com/kcjengr/qtpyvcp.git:gh-pages

---
+++ _static/documentation_options.js
@@ -0,0 +1,10 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'v0.2.3+87.g5a23c4b',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+};
